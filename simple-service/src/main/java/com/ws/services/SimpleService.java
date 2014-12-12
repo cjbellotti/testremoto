eb39ace4572e4f9d0b74c6c296a205c7fa144627
Carlos <carlos@mail.com>
2014-12-12 12:16:52 -0300

    Inicializacion del proyecto

---
+++ simple-service/src/main/java/com/ws/services/SimpleService.java
@@ -0,0 +1,22 @@
+package com.ws.services;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public class SimpleService {
+
+	@WebMethod(operationName = "sayHello")
+	public String sayHello(@WebParam(name="guestname") String guestname) {
+
+		if(guestname == null) {
+			return "Hello";
+		}
+
+		return "Hello " + guestname;
+
+	}
+}
